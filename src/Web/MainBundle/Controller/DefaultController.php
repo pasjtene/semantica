2fbaabd3f11496e7effc3bb0eb62800b42558290
Danick Takam <Sdanicktakam@yahoo.fr>
2017-04-15 22:46:11 +0100

    Envoi  du  mail pour le contact,  modification de la vue et du  footer

--- src/Web/MainBundle/Controller/DefaultController.php
+++ src/Web/MainBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Web\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -21,5 +22,33 @@ class DefaultController extends Controller
     public function signupAction()
     {
         return $this->render('MainBundle:Default:signup.html.twig');
+    }
+
+    /**
+     * @Route("/", name="main_contact")
+     */
+    public function  contactAction(Request $request)
+    {
+        $val = $request->request;
+        $email = $val->get('c_email');
+        $name = $val->get('name');
+        $message = $val->get('message');
+        $code =$this->sendMail($email,$this->getParameter('mailer_user'),$message,"Contact : ".$name);
+        return $this->redirect($this->generateUrl('main_homepage'));
+    }
+
+    public  function sendMail($to, $from, $body,$subjet)
+    {
+        // ->setReplyTo('xxx@xxx.xxx')
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject($subjet)
+            ->setFrom($from) // 'info@achgroupe.com' => 'Achgroupe : Course en ligne '
+            ->setTo($to)
+            ->setBody($body)
+            //'MyBundle:Default:mail.html.twig'
+            ->setContentType('text/html');
+        return $this->get('mailer')->send($message);
+
     }
 }
