aa79c4aa2c893fa77f64865eedae334170d3e8df
Stratege takam <Sdanicktakam@yahoo.fr>
2017-04-26 00:13:02 +0100

    Gestion de la suppression d'un projet avec tous ces fichiers depuis la partir admin

--- src/Web/AdminBundle/Controller/ProjectController.php
+++ src/Web/AdminBundle/Controller/ProjectController.php
@@ -4,6 +4,9 @@ namespace Web\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Web\EntityBundle\Entity\Files;
+use Web\EntityBundle\Entity\Projet;
+
 /**
  * @Route("/projet")
  */
@@ -18,5 +21,25 @@ class ProjectController extends Controller
         $list = $em->getRepository("EntityBundle:Projet")->findBy([],['id'=>'DESC']);
         $array['list'] = $list;
         return $this->render('AdminBundle:Project:index.html.twig',$array);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="admin_projet_delete", requirements={"id": "\d+"})
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        /** @var Projet $project */
+        $project = $em->getRepository("EntityBundle:Projet")->find($id);
+        $files = new Files();
+        if($project->getFiles()!=null)
+        {
+            $files->delete('',$project->path());
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($project);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('admin_homepage'));
     }
 }
