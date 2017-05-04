a6df86a18be09b1d97de92c922bea57ca50292e9
Stratege takam <Sdanicktakam@yahoo.fr>
2017-05-04 10:50:18 +0100

    Creation des entités Configuration des multiplicités Gestion de la traduction Validation des entités

---
+++ src/Web/EntityBundle/Entity/Suggestion.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Web\EntityBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+/**
+ * Suggestion
+ *
+ * @ORM\Table(name="suggestion")
+ * @ORM\Entity(repositoryClass="Web\EntityBundle\Repository\SuggestionRepository")
+ */
+class Suggestion extends BaseInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @Assert\Valid()
+     * @ORM\ManyToOne(targetEntity="Web\EntityBundle\Entity\Person",cascade={"persist"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $person;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="status", type="boolean")
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * Set status
+     *
+     * @param boolean $status
+     *
+     * @return Suggestion
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return boolean
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set person
+     *
+     * @param \Web\EntityBundle\Entity\Person $person
+     *
+     * @return Suggestion
+     */
+    public function setPerson(\Web\EntityBundle\Entity\Person $person)
+    {
+        $this->person = $person;
+
+        return $this;
+    }
+
+    /**
+     * Get person
+     *
+     * @return \Web\EntityBundle\Entity\Person
+     */
+    public function getPerson()
+    {
+        return $this->person;
+    }
+}
