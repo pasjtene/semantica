727151e2e821e7611dc0345779a15c2af1d8f16f
Stratege takam <Sdanicktakam@yahoo.fr>
2017-05-10 02:03:46 +0100

    Mise ajour des fichiers

---
+++ src/Web/EntityBundle - Copy/Entity/Planning.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Web\EntityBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+/**
+ * Planning
+ *
+ * @ORM\Table(name="planning")
+ * @ORM\Entity(repositoryClass="Web\EntityBundle\Repository\PlanningRepository")
+ */
+class Planning
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @Assert\Valid()
+     * @ORM\ManyToOne(targetEntity="Web\EntityBundle\Entity\Projet",cascade={"persist"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $project;
+
+    /**
+     * @var \DateTime
+     * @Assert\NotBlank(message="historic.startdate.NotBlank")
+     * @ORM\Column(name="startdate", type="datetime")
+     */
+    private $startdate;
+
+    /**
+     * @var string
+     * @Assert\NotBlank(message="historic.endate.NotBlank")
+     * @ORM\Column(name="enddate", type="string", length=255)
+     */
+    private $enddate;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set startdate
+     *
+     * @param \DateTime $startdate
+     *
+     * @return Planning
+     */
+    public function setStartdate($startdate)
+    {
+        $this->startdate = $startdate;
+
+        return $this;
+    }
+
+    /**
+     * Get startdate
+     *
+     * @return \DateTime
+     */
+    public function getStartdate()
+    {
+        return $this->startdate;
+    }
+
+    /**
+     * Set enddate
+     *
+     * @param string $enddate
+     *
+     * @return Planning
+     */
+    public function setEnddate($enddate)
+    {
+        $this->enddate = $enddate;
+
+        return $this;
+    }
+
+    /**
+     * Get enddate
+     *
+     * @return string
+     */
+    public function getEnddate()
+    {
+        return $this->enddate;
+    }
+
+    /**
+     * Set project
+     *
+     * @param \Web\EntityBundle\Entity\Projet $project
+     *
+     * @return Planning
+     */
+    public function setProject(\Web\EntityBundle\Entity\Projet $project)
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    /**
+     * Get project
+     *
+     * @return \Web\EntityBundle\Entity\Projet
+     */
+    public function getProject()
+    {
+        return $this->project;
+    }
+}
---
+++ src/Web/EntityBundle - Copy/Form/AdditionType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Web\EntityBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdditionType extends AbstractType
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('code',TextType::class,array(
+                'label' => 'form.commit.code',
+                'translation_domain' => 'forms',
+                'required'    => true
+            ))
+            ->add('description',TextareaType::class,array(
+                'label' => 'form.base.description',
+                'translation_domain' => 'forms',
+                'required'    => true
+            ))
+            ->add('date',DateTimeType::class,array(
+                'label' => 'form.base.date',
+                'translation_domain' => 'forms',
+                'required'    => true
+            ))
+            ->add('participator',EntityType::class,array(
+                'class'=>"EntityBundle:Participator",
+                'property'=>'code',
+                'required'=>true,
+                'multiple'=>false,
+                'translation_domain' => 'forms',
+                'empty_data'=>null
+            )); // 'empty_value'=>'form.base.empty_participator',
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Web\EntityBundle\Entity\Addition'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'web_entitybundle_addition';
+    }
+
+
+}
