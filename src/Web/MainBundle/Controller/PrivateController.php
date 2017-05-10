a901be33a14b7cc177972c64dacc627292c49cd8
Stratege takam <Sdanicktakam@yahoo.fr>
2017-05-10 01:43:37 +0100

    Merge de ma branch et master

--- src/Web/MainBundle/Controller/PrivateController.php
+++ src/Web/MainBundle/Controller/PrivateController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Web\MainBundle\Controller;
-
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -12,53 +10,119 @@ use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Exception\AccountStatusException;
 use Web\EntityBundle\Entity\Contact;
 use Web\EntityBundle\Entity\Customer;
+use Web\EntityBundle\Entity\Projet;
 use Web\EntityBundle\Entity\User;
 use Web\EntityBundle\Form\UserType;
-
 /**
  * @Route("/private")
  */
 class PrivateController extends Controller
 {
-
-
-
     /**
      * @Route("/", name="main_private")
      */
     public function indexAction(Request $request)
     {
         /** @var User $user */
-        $user = $this->container->get('security.token_storage')->getToken()->getUser();
-
-
+        $user = $this->getUser();
         $em = $this->getDoctrine()->getManager();
-        $list = $em->getRepository("EntityBundle:Projet")->findBy(['email'=>$user->getEmail()],['id'=>'DESC','time'=>'DESC']);
+        $all= $em->getRepository("EntityBundle:Projet")->findAll();
+        if($all !=null)
+        {
+            /** @var Projet $item */
+            foreach($all as $item)
+            {
+                if($item->getVisitor()!=null && ($item->getVisitor()->getEmail()==$user->getEmail() || $item->getVisitor()->getPhone()== $user->getUsername()))
+                {
+                    $item->setUser($user);
+                    $em->flush();
+                    $em->detach($item);
+                }
+            }
+        }
+        $list = $em->getRepository("EntityBundle:Projet")->findBy(['user'=>$user],['id'=>'DESC','date'=>'DESC']);
         $array['list'] = $list;
-
+        $array['index'] = 1;
+        return $this->render('MainBundle:Private:index.html.twig',$array);
+    }
+    /**
+     * @Route("/commit", name="main_private_commit")
+     */
+    public function commitAction(Request $request)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $all= $em->getRepository("EntityBundle:Projet")->findAll();
+        if($all !=null)
+        {
+            /** @var Projet $item */
+            foreach($all as $item)
+            {
+                if($item->getVisitor()!=null && ($item->getVisitor()->getEmail()==$user->getEmail() || $item->getVisitor()->getPhone()== $user->getUsername()))
+                {
+                    $item->setUser($user);
+                    $em->flush();
+                    $em->detach($item);
+                }
+            }
+        }
+        $list = $em->getRepository("EntityBundle:Projet")->findBy(['user'=>$user],['id'=>'DESC','date'=>'DESC']);
+        $array['list'] = $list;
+        $array['index'] =3;
+        return $this->render('MainBundle:Private:commit.html.twig',$array);
+    }
+    /**
+     * @Route("/comment", name="main_private_comment")
+     */
+    public function commentAction(Request $request)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $all= $em->getRepository("EntityBundle:Projet")->findAll();
+        if($all !=null)
+        {
+            /** @var Projet $item */
+            foreach($all as $item)
+            {
+                if($item->getVisitor()!=null && ($item->getVisitor()->getEmail()==$user->getEmail() || $item->getVisitor()->getPhone()== $user->getUsername()))
+                {
+                    $item->setUser($user);
+                    $em->flush();
+                    $em->detach($item);
+                }
+            }
+        }
+        $list = $em->getRepository("EntityBundle:Projet")->findBy(['user'=>$user],['id'=>'DESC','date'=>'DESC']);
+        $array['list'] = $list;
+        $array['index'] = 4;
+        return $this->render('MainBundle:Private:comment.html.twig',$array);
+    }
+    /**
+     * @Route("/profile", name="main_private_profile")
+     */
+    public function profileAction(Request $request)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
         $objet = $em->getRepository("EntityBundle:User")->find($user->getId());
-
         /** @var Form $form */
         $form = $this->get("form.factory")->create(UserType::class,$objet);
-
         if($request->isMethod("POST"))
         {
-
             $form->handleRequest($request);
             $em = $this->getDoctrine()->getManager();
             $objet->setEnabled(true);
             $objet->setPassword($user->getPassword());
             $objet->setRoles(['ROLE_USER']);
-
             /** @var Validator $validator */
             $validator = $this->get('validator');
             $error = $validator->validate($objet);
             if(count($error) == 0)
             {
-                $password = $this->encodePassword(new User(), $objet->getPassword(), $objet->getSalt());
-
-                $objet->setPassword($password);
-
+                $objet->setPassword($request->request->get('password'));
                 $em->persist($objet);
                 $em->flush();
                 $em->detach($objet);
@@ -68,38 +132,27 @@ class PrivateController extends Controller
                 $array['error'] = $error;
                 //var_dump($error);
             }
-
-
         }
-
         $array['form'] = $form->createView();
         $array['objet'] = $objet;
-
-        return $this->render('MainBundle:Private:index.html.twig',$array);
+        $array['index'] = 2;
+        return $this->render('MainBundle:Private:profile.html.twig',$array);
     }
-
-
     public function encodePassword($object, $password, $salt)
     {
         $factory = $this->get('security.encoder_factory');
         $encoder = $factory->getEncoder($object);
         $password = $encoder->encodePassword($password, $salt);
-
         return $password;
     }
-
-
     public function authenticateUser(UserInterface $user)
     {
         try {
-
             $tocken = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
             $this->get('security.token_storage')->setToken($tocken);
             $this->get('session')->set('_security_main',serialize($tocken));
-
         } catch (AccountStatusException $ex) {
             //var_dump($ex->getMessage());
         }
     }
-
 }
--- src/Web/MainBundle/Controller/ProjectController.php
+++ src/Web/MainBundle/Controller/ProjectController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Web\MainBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Config\Tests\Util\Validator;
@@ -9,8 +7,9 @@ use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Web\EntityBundle\Entity\Files;
 use Web\EntityBundle\Entity\Projet;
+use Web\EntityBundle\Entity\User;
+use Web\EntityBundle\Entity\Visitor;
 use Web\EntityBundle\Form\ProjetType;
-
 /**
  * @Route("/project")
  */
@@ -21,19 +20,14 @@ class ProjectController extends Controller
      */
     public function indexAction(Request $request)
     {
-
-
         $objet = new Projet();
-
         /** @var Form $form */
         $form = $this->get("form.factory")->create(ProjetType::class,$objet);
-
         if($request->isMethod('POST'))
         {
             $form->handleRequest($request);
-
             $em = $this->getDoctrine()->getManager();
-            $objet->setTime(new \DateTime());
+            $objet->setDate(new \DateTime());
             $file = new Files();
             if ($objet->getFile() != null) {
                 $file->file = $objet->getFile();
@@ -43,23 +37,53 @@ class ProjectController extends Controller
                 $objet->setHashfiles(uniqid().'.'.$objet->getExtfiles());
                 $file->add($file->initialpath."projet",  $objet->getHashfiles());
             }
-
+            $objet->setCode(uniqid())->setState(true)->setStatus("En cours")->getUser()->setRoles(['ROLE_USER'])->setEnabled(true)->setPassword("test")->setPleasantries("M.");
+            /** @var User $user */
+            $user =$em->getRepository('EntityBundle:User')->findOneByemail($objet->getUser()->getEmail());
+            if($user !=null)
+            {
+                $objet->setUser($user);
+            }
+            else if($user =$em->getRepository('EntityBundle:User')->findOneByusername($objet->getUser()->getUsername()) !=null)
+            {
+                $objet->setUser($user);
+            }
+            else{
+                //$ip = $request->getClientIp();
+                $ip = $_SERVER['REMOTE_ADDR'];
+                $vistor = new Visitor();
+                $vistor->setPleasantries("M.");
+                $vistor->setCity($objet->getUser()->getCity());
+                $vistor->setCountry($objet->getUser()->getCountry());
+                $vistor->setPhone($objet->getUser()->getUsername());
+                $vistor->setEmail($objet->getUser()->getEmail());
+                $vistor->setFirstname($objet->getUser()->getFirstname());
+                $vistor->setIp($ip);
+                $visitorcurrent =$em->getRepository('EntityBundle:Visitor')->findOneByemail($objet->getUser()->getEmail());
+                if($visitorcurrent !=null)
+                {
+                    $vistor = $visitorcurrent;
+                }
+                else if ($visitorcurrent =$em->getRepository('EntityBundle:Visitor')->findOneBphone($objet->getUser()->getUsername())!=null)
+                {
+                    $vistor = $visitorcurrent;
+                }
+                $objet->setUser(null);
+                $objet->setVisitor($vistor);
+            }
             /** @var Validator $validator */
             $validator = $this->get('validator');
             $error = $validator->validate($objet);
             if(count($error) == 0)
             {
+                $email = $objet->getUser()->getEmail();
                 $em->persist($objet);
                 $em->flush();
-
                 $em->detach($objet);
-
-
                 $translator = $this->get('translator');
                 $locale = $this->get('session')->get('_locale');
                 $message = $translator->trans('form.project.notification',[] ,'forms', $locale);
-                $code = $this->sendMail($objet->getEmail(), $this->getParameter('mailer_user'), $message, "SOMMIT PROJET STC(SEMANTICA TECHNOLOGIES CORPORATION)");
-
+                $code = $this->sendMail($email, $this->getParameter('mailer_user'), $message, "SOMMIT PROJET STC(SEMANTICA TECHNOLOGIES CORPORATION)");
                 $objet = new Projet();
                 /** @var Form $form */
                 $form = $this->get("form.factory")->create(ProjetType::class,$objet);
@@ -67,20 +91,26 @@ class ProjectController extends Controller
             }
             else{
                 $array['error'] = $error;
-                //var_dump($error);
+                // var_dump($error);
             }
-
+        }
+        /** @var User $user */
+        $user = $this->getUser();
+        if($user!=null)
+        {
+            //var_dump($user);
+            $em = $this->getDoctrine()->getManager();
+            /** @var User $user */
+            $user =$em->getRepository('EntityBundle:User')->find($user->getId());
+            $objet->setUser($user);
         }
         $array['form'] = $form->createView();
         $array['objet'] = $objet;
         return $this->render('MainBundle:Project:index.html.twig',$array);
     }
-
-
     public  function sendMail($to, $from, $body,$subjet)
     {
         // ->setReplyTo('xxx@xxx.xxx')
-
         $message = \Swift_Message::newInstance()
             ->setSubject($subjet)
             ->setFrom($from) // 'info@achgroupe.com' => 'Achgroupe : Course en ligne '
@@ -89,7 +119,5 @@ class ProjectController extends Controller
             //'MyBundle:Default:mail.html.twig'
             ->setContentType('text/html');
         return $this->get('mailer')->send($message);
-
     }
-
 }
