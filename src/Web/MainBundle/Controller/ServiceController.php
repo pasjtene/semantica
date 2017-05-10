b268e5abd85aeb4814454ca8757e804b959bbfc9
Stratege takam <Sdanicktakam@yahoo.fr>
2017-05-10 10:34:40 +0100

    Petite correction sur sercices et  tous ces menus Suppression des fichiers unitiles Compression du  code

--- src/Web/MainBundle/Controller/ServiceController.php
+++ src/Web/MainBundle/Controller/ServiceController.php
@@ -8,13 +8,37 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 /**
  * @Route("/service")
  */
-class serviceController extends Controller
+class ServiceController extends Controller
 {
     /**
      * @Route("/", name="main_service")
      */
     public function indexAction()
     {
-        return $this->render('MainBundle:services:index.html.twig');
+        return $this->render('MainBundle:Service:index.html.twig');
+    }
+
+    /**
+     * @Route("/business", name="main_business")
+     */
+    public function businessAction()
+    {
+        return $this->render('MainBundle:Service:business.html.twig');
+    }
+
+    /**
+     * @Route("/application", name="main_application")
+     */
+    public function applicationAction()
+    {
+        return $this->render('MainBundle:Service:application.html.twig');
+    }
+
+    /**
+     * @Route("/inf", name="main_inf")
+     */
+    public function infAction()
+    {
+        return $this->render('MainBundle:Service:inf.html.twig');
     }
 }
