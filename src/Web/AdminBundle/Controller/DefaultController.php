0f30b786458d6a14031dd6932a71003eb5be48f5
Stratege takam <Sdanicktakam@yahoo.fr>
2017-05-16 00:18:36 +0100

    Liste des projets backend

--- src/Web/AdminBundle/Controller/DefaultController.php
+++ src/Web/AdminBundle/Controller/DefaultController.php
@@ -19,7 +19,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $list = $em->getRepository("EntityBundle:Projet")->findBy([],['id'=>'DESC','time'=>'DESC']);
+        $list = $em->getRepository("EntityBundle:Projet")->findBy([],['id'=>'DESC','date'=>'DESC']);
         $array['list'] = $list;
         return $this->render('AdminBundle:Default:index.html.twig',$array);
     }
