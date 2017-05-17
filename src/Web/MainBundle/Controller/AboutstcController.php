2dc73fcbfa964dddb84085451b5d18cd14ad55e1
TAJORE <jouanirenatot@gmail.com>
2017-05-17 20:50:12 +0200

    update_about

---
+++ src/Web/MainBundle/Controller/AboutstcController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Web\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/aboutstc")
+ */
+class AboutstcController extends Controller
+{
+    /**
+     * @Route("/", name="main_aboutstc")
+     */
+    public function indexAction()
+    {
+        return $this->render('MainBundle:aboutstc:index.html.twig');
+    }
+
+    /**
+     * @Route("/business", name="main_business")
+     */
+    public function businessAction()
+    {
+        return $this->render('MainBundle:Service:business.html.twig');
+    }
+
+    /**
+     * @Route("/application", name="main_application")
+     */
+    public function applicationAction()
+    {
+        return $this->render('MainBundle:Service:application.html.twig');
+    }
+
+    /**
+     * @Route("/inf", name="main_inf")
+     */
+    public function infAction()
+    {
+        return $this->render('MainBundle:Service:inf.html.twig');
+    }
+    /**
+     * @Route("/exploreservice", name="main_exploreservice")
+     */
+    public function exploreserviceAction()
+    {
+        return $this->render('MainBundle:Service:exploreservice.html.twig');
+    }
+
+}
