ebb21c334a77f1212df210dcf59c088e0d3ea959
NGUIMOU <nguikoual@yahoo.fr>
2017-05-21 20:29:07 +0200

    Implémentation de l'ajout d'un participant, d'un planning et d'une tâche ! Listes des particpants, planning et tâches

--- src/Web/EntityBundle/Entity/User.php
+++ src/Web/EntityBundle/Entity/User.php
@@ -3,7 +3,7 @@
 namespace Web\EntityBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
@@ -229,15 +229,25 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
         return $this->picture;
     }
 
-      /**
-       * Get all roles of application
-       *
-       * @return array
-       */
+   /**
+    * Get all roles of application
+    *
+    * @return array
+    */
     public static function getAppRole()
     {
         $array = [self::ROLE_USER, self::ROLE_STAFF, self::ROLE_ADMIN, self::ROLE_SUPER_ADMIN];
 
         return $array;
     }
+
+      /**
+       * Get user's role as string
+       *
+       * @return string
+       */
+      public function getRoleString()
+      {
+          return implode(", ", $this->roles);
+      }
 }
